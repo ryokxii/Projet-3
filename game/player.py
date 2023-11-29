ebf41ee137888f03656b459c24d6a65b11797980
Zafer A <lssjshisui@gmail.com>
2023-11-29 14:09:12 -0500

    2e video fini

--- game/player.py
+++ game/player.py
@@ -1,9 +1,10 @@
 import pygame
 from support import import_folder
+from math import sin
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, surface, create_jump_particles):
+    def __init__(self, pos, surface, create_jump_particles, change_health):
         super().__init__()
         self.import_character_assets()
         self.frame_index = 0
@@ -18,19 +19,31 @@ class Player(pygame.sprite.Sprite):
         self.display_surface = surface
         self.create_jump_particles = create_jump_particles
 
-        # mouvement
+        # player movement
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 8
         self.gravity = 0.8
         self.jump_speed = -16
+        self.collision_rect = pygame.Rect(self.rect.topleft, (50, self.rect.height))
 
-        # joueur
+        # player status
         self.status = "idle"
         self.facing_right = True
         self.on_ground = False
-        self.on_ceilling = False
+        self.on_ceiling = False
         self.on_left = False
         self.on_right = False
+
+        # health management
+        self.change_health = change_health
+        self.invincible = False
+        self.invincibility_duration = 500
+        self.hurt_time = 0
+
+        # audio
+        self.jump_sound = pygame.mixer.Sound("audio/effects/jump.wav")
+        self.jump_sound.set_volume(0.5)
+        self.hit_sound = pygame.mixer.Sound("audio/effects/hit.wav")
 
     def import_character_assets(self):
         character_path = "graphics/character/"
@@ -46,6 +59,7 @@ class Player(pygame.sprite.Sprite):
     def animate(self):
         animation = self.animations[self.status]
 
+        # loop over frame index
         self.frame_index += self.animation_speed
         if self.frame_index >= len(animation):
             self.frame_index = 0
@@ -53,25 +67,19 @@ class Player(pygame.sprite.Sprite):
         image = animation[int(self.frame_index)]
         if self.facing_right:
             self.image = image
+            self.rect.bottomleft = self.collision_rect.bottomleft
         else:
             flipped_image = pygame.transform.flip(image, True, False)
             self.image = flipped_image
+            self.rect.bottomright = self.collision_rect.bottomright
 
-        # set nouveau rect dependant de la collision
-        if self.on_ground:
-            self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
-        elif self.on_ground and self.on_right:
-            self.rect = self.image.get_rect(bottomright=self.rect.bottomright)
-        elif self.on_ground and self.on_left:
-            self.rect = self.image.get_rect(bottomleft=self.rect.bottomleft)
-        elif self.on_ceilling:
-            self.rect = self.image.get_rect(midtop=self.rect.midtop)
-        elif self.on_ceilling and self.on_right:
-            self.rect = self.image.get_rect(topright=self.rect.topright)
-        elif self.on_ceilling and self.on_left:
-            self.rect = self.image.get_rect(topleft=self.rect.topleft)
+        if self.invincible:
+            alpha = self.wave_value()
+            self.image.set_alpha(alpha)
         else:
-            self.rect = self.image.get_rect(center=self.rect.center)
+            self.image.set_alpha(255)
+
+        self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
 
     def run_dust_animation(self):
         if self.status == "run" and self.on_ground:
@@ -84,7 +92,7 @@ class Player(pygame.sprite.Sprite):
             if self.facing_right:
                 pos = self.rect.bottomleft - pygame.math.Vector2(6, 10)
                 self.display_surface.blit(dust_particle, pos)
-            elif self.facing_right is False:
+            else:
                 pos = self.rect.bottomright - pygame.math.Vector2(6, 10)
                 flipped_dust_particle = pygame.transform.flip(
                     dust_particle, True, False
@@ -100,16 +108,17 @@ class Player(pygame.sprite.Sprite):
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
             self.facing_right = False
-        elif keys[pygame.K_SPACE] and self.on_ground:
-            self.jump()
-            self.create_jump_particles(self.rect.midbottom)
         else:
             self.direction.x = 0
+
+        if keys[pygame.K_SPACE] and self.on_ground:
+            self.jump()
+            self.create_jump_particles(self.rect.midbottom)
 
     def get_status(self):
         if self.direction.y < 0:
             self.status = "jump"
-        elif self.direction.y > self.gravity:
+        elif self.direction.y > 1:
             self.status = "fall"
         else:
             if self.direction.x != 0:
@@ -119,13 +128,36 @@ class Player(pygame.sprite.Sprite):
 
     def apply_gravity(self):
         self.direction.y += self.gravity
-        self.rect.y += self.direction.y
+        self.collision_rect.y += self.direction.y
 
     def jump(self):
         self.direction.y = self.jump_speed
+        self.jump_sound.play()
+
+    def get_damage(self):
+        if not self.invincible:
+            self.hit_sound.play()
+            self.change_health(-10)
+            self.invincible = True
+            self.hurt_time = pygame.time.get_ticks()
+
+    def invincibility_timer(self):
+        if self.invincible:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.hurt_time >= self.invincibility_duration:
+                self.invincible = False
+
+    def wave_value(self):
+        value = sin(pygame.time.get_ticks())
+        if value >= 0:
+            return 255
+        else:
+            return 0
 
     def update(self):
         self.get_input()
         self.get_status()
         self.animate()
         self.run_dust_animation()
+        self.invincibility_timer()
+        self.wave_value()
--- game/settings.py
+++ game/settings.py
@@ -1,23 +1,8 @@
-level_map = [
-    "                            ",
-    "                            ",
-    "                            ",
-    " XX    XXX            XX    ",
-    " XX P                       ",
-    " XXXX         XX         XX ",
-    " XXXX       XX              ",
-    " XX    X  XXXX    XX  XX    ",
-    "       X  XXXX    XX  XXX   ",
-    "    XXXX  XXXXXX  XX  XXXX  ",
-    "XXXXXXXX  XXXXXX  XX  XXXX  ",
-]
-# 'X' : tile
-# 'P' : Player spawn point
-# add extra like coins, ennemies, etc
-
+vertical_tile_number = 11
 tile_size = 64
+
+screen_height = vertical_tile_number * tile_size
 screen_width = 1200
-screen_height = len(level_map) * tile_size
 
 # game statuses
 INTRO = 0
